71a94a522688da45db0a15f7bd10da12c5eb5cf2
Jonjo7Dia <jonosphgames@gmail.com>
2023-04-18 18:32:24 +0200

    Add home page to config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "162ba9f659893610"; }
+/******/ 		__webpack_require__.h = function() { return "4c336ad76253ed61"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
